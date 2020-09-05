b69fb7b92c1abb9041d05c0c79f4a3e191fd18fb
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-09-05 15:57:35 -0300

    Add api and create get all entries endpoint

---
+++ pages/api/avo.ts
@@ -0,0 +1,15 @@
+import {} from 'https'
+import { IncomingHttpHeaders, ServerResponse } from 'http'
+import DB from '@database'
+
+const allAvos = async (req: IncomingHttpHeaders, res: ServerResponse) => {
+  const db = new DB()
+  const allEntries = await db.getAll()
+  const length = allEntries.length
+
+  res.statusCode = 200
+  res.setHeader('Content-type', 'application/json')
+  res.end(JSON.stringify({ data: allEntries, length }))
+}
+
+export default allAvos
